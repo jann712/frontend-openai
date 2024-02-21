04dbb1f4450caf6884e032494ebd4c246bfcfabf
jann712 <joao.augustonn1@gmail.com>
2024-02-21 15:27:35 -0300

    readme update + small code changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import Header from "./Header";
 import MainSection from "./MainSection";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <Header />
